8393912db712cc2d1de8a724bf86cfde214aac47
devlozano <lozanoemiliobien@gmail.com>
2026-06-07 23:01:03 +0800

    Configure live icteach-free Firebase web integration parameters

--- lib/main.dart
+++ lib/main.dart
@@ -16,15 +16,16 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   if (kIsWeb) {
-    // Web Configuration Setup
+    // Web Configuration Setup mapped to your icteach-free credentials
     await Firebase.initializeApp(
       options: const FirebaseOptions(
-        apiKey: "AIzaSyYourActualAPIKeyHere_XYZ",
-        appId: "1:1234567890:web:abcdef123456",
-        messagingSenderId: "1234567890",
-        projectId: "icteach-project-id",
-        authDomain: "icteach-project-id.firebaseapp.com",
-        storageBucket: "icteach-project-id.appspot.com",
+        apiKey: "AIzaSyDCnNrIz2g2Ovq5XnocJVBVl5S065vOB2g",
+        authDomain: "icteach-free.firebaseapp.com",
+        projectId: "icteach-free",
+        storageBucket: "icteach-free.firebasestorage.app",
+        messagingSenderId: "4824580226",
+        appId: "1:4824580226:web:e28624a19f13361241f49b",
+        measurementId: "G-S77WYVMF0N", // Included your measurement id tracking
       ),
     );
   } else {
@@ -53,7 +54,7 @@ class MyApp extends StatelessWidget {
 }
 
 class _AppEntry extends StatelessWidget {
-  _AppEntry();
+  _AppEntry(); // Removed const because _navKey is not a compile-time constant
 
   final GlobalKey<NavigatorState> _navKey = GlobalKey<NavigatorState>();
 
